0c0d9fe0f7cd75eaed2ad178d58631fd4b284eb8
Filip Waligórski <filip.waligorski@student.put.poznan.pl>
2017-03-30 13:54:13 +0200

    installed localforage for angular

--- webtorrent-client/test/karma.conf.js
+++ webtorrent-client/test/karma.conf.js
@@ -30,6 +30,8 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-scroll-glue/src/scrollglue.js',
+      'bower_components/localforage/dist/localforage.js',
+      'bower_components/angular-localforage/dist/angular-localForage.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
